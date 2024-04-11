063ef43d49090b19760b39e216097e2198b415c2
Austin Mitchell <austin@center-oak.com>
2024-04-11 11:27:28 -0400

    added plugins

--- lua/macky/lazy.lua
+++ lua/macky/lazy.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("macky.plugins")
+require("lazy").setup({ {import= "macky.plugins" }, {import= "macky.plugins.lsp"}}, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
